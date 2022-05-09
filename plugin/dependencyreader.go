599d6be2c465e06f21981350a21e8088f998ab28
Carl Henrik Lunde <chlunde@gmail.com>
2022-05-09 14:23:49 +0200

    dependencyreader: Split search into smaller segments, cache results, refresh in background

--- plugin/dependencyreader.go
+++ plugin/dependencyreader.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/chlunde/humio-jaeger-storage/humio"
@@ -16,6 +17,11 @@ import (
 func (h *HumioPlugin) DependencyReader() dependencystore.Reader {
 	if h.dependencyReader == nil {
 		h.dependencyReader = &humioDependencyReader{plugin: h, client: h.getClient(h.ReadToken)}
+		go func() {
+			h.dependencyReader.refreshDependencies()
+			time.Sleep(90 * time.Minute)
+		}()
+
 	}
 	return h.dependencyReader
 }
@@ -23,48 +29,85 @@ func (h *HumioPlugin) DependencyReader() dependencystore.Reader {
 type humioDependencyReader struct {
 	plugin *HumioPlugin
 	client *humio.Client
+
+	cache     []model.DependencyLink
+	cacheLock sync.Mutex
 }
 
-func (h *humioDependencyReader) GetDependencies(ctx context.Context, endTs time.Time, lookback time.Duration) ([]model.DependencyLink, error) {
-	span, ctx := opentracing.StartSpanFromContext(ctx, "GetDependencies")
-	defer span.Finish()
-
+func (h *humioDependencyReader) refreshDependencies() error {
 	var results []struct {
 		Child  string `json:"child"`
 		Parent string `json:"parent"`
 		Count  string `json:"_count"`
 	}
 
-	if err := h.client.QueryDecode(ctx, h.plugin.Repo, humio.Q{
-		// QueryString: "parseJson(payload) | groupBy([#service,peer.service,span.kind])",
-		QueryString: `parseJson(payload) | child:=process.service_name | parent_span_id := references[0].span_id
+	h.plugin.Logger.Warn("refreshDependencies")
+	defer func() {
+		h.plugin.Logger.Warn("refreshDependencies done")
+	}()
+
+	start := time.Now()
+	delta := time.Minute * 15
+
+	type key struct {
+		child  string
+		parent string
+	}
+	hits := make(map[key]model.DependencyLink)
+
+	for i := 0; i < int((24*time.Hour)/delta); i++ {
+		partStart := start.Add(time.Duration(i+1) * -delta)
+		partEnd := start.Add(time.Duration(i) * -delta)
+		h.plugin.Logger.Warn("refreshDependencies subquery", "partStart", partStart, "partEnd", partEnd)
+		if err := h.client.QueryDecode(context.Background(), h.plugin.Repo, humio.Q{
+			// QueryString: "parseJson(payload) | groupBy([#service,peer.service,span.kind])",
+			QueryString: `parseJson(payload) | child:=process.service_name | parent_span_id := references[0].span_id
 | join({parseJson(payload) | parent := process.service_name }, key=[span_id], field=[parent_span_id], include=[parent]) | groupBy([parent,child])`,
-		Start: humio.RelativeTime("1 day"),
-		End:   humio.AbsoluteTime(endTs),
-	}, &results); err != nil {
-		return nil, err
+			Start: humio.AbsoluteTime(partStart),
+			End:   humio.AbsoluteTime(partEnd),
+		}, &results); err != nil {
+			return err
+		}
+
+		for _, res := range results {
+			if res.Child == res.Parent {
+				continue
+			}
+
+			count, err := strconv.ParseUint(res.Count, 10, 64)
+			if err != nil {
+				return fmt.Errorf("unparsable _count from humio: %q (%+v)", res.Count, res)
+			}
+
+			k := key{res.Child, res.Parent}
+			hit := hits[k]
+			hit.Child = res.Child
+			hit.Parent = res.Parent
+			hit.CallCount += count
+			hit.Source = "humio"
+			hits[k] = hit
+		}
 	}
 
 	var ret []model.DependencyLink
-	for _, res := range results {
-		if res.Child == res.Parent {
-			continue
-		}
-
-		count, err := strconv.ParseUint(res.Count, 10, 64)
-		if err != nil {
-			return nil, fmt.Errorf("unparsable _count from humio: %q (%+v)", res.Count, res)
-		}
-
-		ret = append(ret, model.DependencyLink{
-			Parent:    res.Parent,
-			Child:     res.Child,
-			CallCount: count,
-			Source:    "humio",
-		})
+	for _, res := range hits {
+		ret = append(ret, res)
 	}
 
-	return ret, nil
+	h.cacheLock.Lock()
+	defer h.cacheLock.Unlock()
+	h.cache = ret
+	return nil
+}
+
+func (h *humioDependencyReader) GetDependencies(ctx context.Context, endTs time.Time, lookback time.Duration) ([]model.DependencyLink, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "GetDependencies")
+	defer span.Finish()
+
+	h.cacheLock.Lock()
+	defer h.cacheLock.Unlock()
+
+	return h.cache, nil
 }
 
 // We satisfy the dependencystore.Reader interface
